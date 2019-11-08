8809c3238dc00e842057c130e65e21b68df3cd94
Alvin Chan <aschan@gmail.com>
2019-11-08 11:21:18 -0800

    Remove `muse` and clean up formatting

--- keymap.c
+++ keymap.c
@@ -15,7 +15,6 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "muse.h"
 
 enum preonic_layers {
   _QWERTY,
@@ -35,7 +34,6 @@ enum preonic_keycodes {
 #include "dynamic_macro.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
   [_QWERTY] = LAYOUT_preonic_1x2uC( \
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,  KC_7,    KC_8,    KC_9,   KC_0,    KC_BSPC,   \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,  KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLASH, \
@@ -75,7 +73,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
       _______, _______, _______, _______, LOWER,   _______, RAISE,   _______, _______, _______, _______  \
       )
-
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -90,8 +87,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_SUPER);
       }
+
       return false;
-      break;
 
     case LOWER:
       if (record->event.pressed) {
@@ -101,8 +98,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
+
       return false;
-      break;
 
     case RAISE:
       if (record->event.pressed) {
@@ -112,8 +109,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
+
       return false;
-      break;
   }
 
   return true;
